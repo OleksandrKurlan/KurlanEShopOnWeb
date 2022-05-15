9466ec2357aef60267c6c72f4bf71c42fc488304
Oleksandr_Kurlan <Oleksandr_Kurlan@epam.com>
2022-05-15 18:09:44 +0300

    Fix swagger

--- src/PublicApi/Program.cs
+++ src/PublicApi/Program.cs
@@ -167,14 +167,7 @@ app.UseCors(CORS_POLICY);
 
 app.UseAuthorization();
 
-if (app.Environment.IsDevelopment())
-{
-    // Enable middleware to serve generated Swagger as a JSON endpoint.
-    app.UseSwagger();
-}
-
-// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
-// specifying the Swagger JSON endpoint.
+app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
